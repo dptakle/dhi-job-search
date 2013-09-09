5c54732a0e13a82d294439cd984570d5ddf5356b
Duke Takle <dptakle@gmail.com>
2013-09-09 08:42:57 -0500

    Hello

---
+++ jobview/src/com/dice/model/JobDetails.java
@@ -0,0 +1,39 @@
+package com.dice.model;
+
+public class JobDetails {
+	public String getJobTitle() {
+		return jobTitle;
+	}
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+	public String getJobDescription() {
+		return jobDescription;
+	}
+	public void setJobDescription(String jobDescription) {
+		this.jobDescription = jobDescription;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getSkills() {
+		return skills;
+	}
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+	public String getDatePosted() {
+		return datePosted;
+	}
+	public void setDatePosted(String datePosted) {
+		this.datePosted = datePosted;
+	}
+	private String jobTitle;
+	private String jobDescription;
+	private String companyName;
+	private String skills;
+	private String datePosted;
+}
